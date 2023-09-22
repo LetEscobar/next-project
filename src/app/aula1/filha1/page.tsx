90a14350a3fdf1fa771b99819c7c51f5b848134a
LetEscobar <leticia.araujo@estudante.ifms.edu.br>
2023-09-21 21:01:42 -0400

    Aula 1

---
+++ src/app/aula1/filha1/page.tsx
@@ -0,0 +1,7 @@
+export default function filha1(){
+  return(
+    <div>
+      <h1>Olá filha 1!</h1>
+    </div>
+  )
+}
---
+++ src/app/aula1/page.tsx
@@ -0,0 +1,7 @@
+export default function Aula1(){
+  return(
+    <div>
+      <h1>Olá aula1</h1>
+    </div>
+  )
+}
